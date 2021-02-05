bfa751f64217f26e1267608220d7d0e4703c7769
Omar Terrazas <terrazas.omar@gmail.com>
2021-02-05 09:47:44 -0600

    Array flat and flatMap, trim and Entries on ES10

---
+++ src/es10/index.js
@@ -0,0 +1,26 @@
+let array = [1,2,3, [1,2,3, [1,2,3]]]
+
+console.log(array.flat(2))
+
+let array = [1,2,3,4,5]
+
+console.log(array.flatMap(value => [value, value *2]))
+
+let hello = '     hello world            '
+console.log(`${hello}!`)
+console.log(`${hello.trimStart()}!`)
+console.log(`${hello.trimEnd()}!`)
+
+try {
+
+} catch {
+    error
+}
+
+let entries = [["name","Omar"],["age",36]]
+console.log(Object.fromEntries(entries))
+
+/* Symbol Objects */
+let mySymbol = 'My Symbol'
+let symbol = Symbol(mySymbol)
+console.log(symbol.description)
