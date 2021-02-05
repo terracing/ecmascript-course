8b5a60913f9dbb80e192eb4153ef1f37402e1de7
Omar Terrazas <terrazas.omar@gmail.com>
2021-02-05 00:00:24 -0600

    Async await on ES8

---
+++ src/es8/async-await.js
@@ -0,0 +1,26 @@
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve('Hello world'), 3000)
+            : reject(new Error('Test Error'))
+    })
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld()
+    console.log(hello)
+}
+
+helloAsync()
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld()
+        console.log(hello)
+    } catch (error) {
+        console.log('An error ocurred')
+        // console.log(error)
+    }
+}
+
+anotherFunction()
