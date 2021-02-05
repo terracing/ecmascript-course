812e0b72a5d2c320aadbae58ec75676220005353
Omar Terrazas <terrazas.omar@gmail.com>
2021-02-04 23:40:22 -0600

    Review to ES7

---
+++ src/es7/index.js
@@ -0,0 +1,13 @@
+let numbers = [1,2,3,7,8]
+
+if(numbers.includes(7)) {
+    console.log('Yes')
+} else {
+    console.log('Don\'t exist')
+}
+
+let base = 4
+let exponent = 8
+let result = base ** exponent
+
+console.log(result)
