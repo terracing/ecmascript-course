7768497f0cbde2b2c6dcf59ab59c39d0f934db07
Omar Terrazas <terrazas.omar@gmail.com>
2021-02-04 22:32:33 -0600

    Multiline, spread operator and let

--- index.js
+++ index.js
@@ -22,3 +22,43 @@ console.log(epicPhrase)
 // ES6
 let epicPhraseES6 = `${hello} ${world}`
 console.log(epicPhraseES6)
+
+let lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" + "Sed cursus id tellus ac sodales. Quisque semper dignissim libero, at varius urna semper eget. Morbi egestas sollicitudin turpis, ac condimentum mauris finibus nec.\n" + "Aliquam nec est mauris. Nullam ante quam, sagittis sit amet diam vel, tincidunt hendrerit nisl. Nullam malesuada dui eu nibh sodales, sit amet mollis sapien tristique. Pellentesque feugiat semper tortor in convallis.'
+
+let loremES6 = `Duis non fringilla nisl. Quisque quis elementum ex. Vivamus interdum libero eget leo dignissim, eu aliquam lorem pharetra. 
+Nunc vel arcu nulla. Morbi commodo elementum nunc. Etiam rhoncus metus neque, at suscipit dolor euismod pretium. Ut eleifend metus erat, id viverra nibh porta vitae. 
+Aliquam efficitur lobortis magna, quis lacinia turpis venenatis vel. In aliquet, elit sed fringilla convallis, sem felis semper nunc, ac sodales lacus urna non lacus. Suspendisse egestas lorem ac rhoncus eleifend.`
+
+console.log(lorem)
+console.log(loremES6)
+
+let person = {
+    'name': 'Omar',
+    'age': 36,
+    'country': 'MX'
+}
+
+console.log(person.name, person.age)
+
+let {name,age,country} = person
+console.log(name, country)
+
+let team1 = ['Daniel','Jaime','Ernesto','Nestor']
+let team2 = ['Elena','Ana','Claudia','Camila']
+
+let education = ['Omar', ...team1, ...team2]
+console.log(education)
+
+{
+    var globalVar = 'Global Var'
+}
+
+{
+    let globalLet = 'Global Let'
+    console.log(globalLet)
+}
+
+console.log(globalVar)
+
+/* let don't work outside of your scope */
+console.log(globalLet)
